c38abbef26340354796371224db2af1055d8e338
Philippe Teuwen <phil@teuwen.org>
2014-05-25 17:29:45 +0200

    Add option pixelwidth

--- ElectronicColoringBook.py
+++ ElectronicColoringBook.py
@@ -8,10 +8,11 @@
 # All but (color-1) ECB will be painted in black
 # Image size and aspect will only be respected if blocksize is multiple of 4
 
-# TODO: propose other modes than 4 bytes pixels
+# TODO: propose other colormaps
 
 import sys, math, random
 from PIL import Image
+import colorsys
 import operator
 from optparse import OptionParser
 
@@ -25,6 +26,7 @@ options.add_option('-y', '--height', type='int', help='Height of output image')
 options.add_option('-s', '--step', type='int', default=100, help='Step when guessing image size. Smaller is slower but more precie, default=100')
 options.add_option('-o', '--offset', type='int', default=0, help='Offset to skip original header, in number of blocks')
 options.add_option('-f', '--flip', action="store_true", default=False, help='Flip image top<>bottom')
+options.add_option('-p', '--pixelwidth', type='int', default=1, help='How many bytes per pixel in the original image')
 
 def histogram(data, blocksize):
     d={}
@@ -67,47 +69,67 @@ histo=histo[:len(histo)/opts.groups*opts.groups]
 if not histo:
     raise NameError("Did not find any single match :-(")
 
-mode="RGBA"
-pxsize=4 # RGBA: 4 bytes per pixel
-pxblocksize=max(opts.blocksize/pxsize, 1)
-A="\xFF" # Alpha channel, if any
+# Construct palette
+N = 254
+HSV_tuples = [(x*1.0/N, 0.8, 0.8) for x in range(N)]
+RGB_tuples = map(lambda x: colorsys.hsv_to_rgb(*x), HSV_tuples)
+p=[0,0,0]               # black
+for rgb in RGB_tuples:
+  p.extend(rgb)         # rainbow
+p.extend([1, 1, 1])     # white
+p=[int(pp*255) for pp in p]
+# Show palette:
+#j=Image.fromstring('P', (256, 256), ''.join([chr(a) for a in range(256)]*256))
+#j.putpalette(p)
+#j.show()
+
 # Let's use random colors...
 colormap={}
 for i in range(len(histo)/opts.groups):
     if i == 0:
-        outtoken=("\xff\xff\xff" + A)* pxblocksize # white
+        color=255 # white
     else:
-        outtoken=(chr(random.randint(1,254)) + chr(random.randint(1,254)) + chr(random.randint(1,254)) + A) * pxblocksize
+        color=random.randint(1,254)
     for g in range(opts.groups):
         gi =(i*opts.groups)+g
-        colormap[histo[gi][0]]=outtoken
-        print "%s %10s #%s" % (histo[gi][0], histo[gi][1], colormap[histo[gi][0]][:3].encode('hex'))
+        colormap[histo[gi][0]]=chr(color)
+        print "%s %10s #%02X -> #%02X #%02X #%02X" % (histo[gi][0], histo[gi][1], color, p[color*3], p[(color*3)+1], p[(color*3)+2])
 blocksleft=len(ciphertext)/opts.blocksize-reduce(lambda x, y: x+y, [n for (t, n) in histo])
-print "%s %10i #%s" % ("*" * len(histo[0][0]), blocksleft, "000000")
+color=0 # black
+print "%s %10i #%02X -> #%02X #%02X #%02X" % ("*" * len(histo[0][0]), blocksleft, color, p[color*3], p[(color*3)+1], p[(color*3)+2])
 
 # Construct output stream
 out=""
+outlen=0
+outlenfloat=0.0
 for i in range(len(ciphertext)/opts.blocksize):
     token=ciphertext[i*opts.blocksize:(i+1)*opts.blocksize].encode('hex')
     if token in colormap:
-        out+=colormap[token]
+        byte=colormap[token]
     else:
-        out+=("\x00\x00\x00" + A)* pxblocksize # black
+        byte=chr(color)
+    out+=byte*(opts.blocksize/opts.pixelwidth)
+    outlen+=opts.blocksize/opts.pixelwidth
+    outlenfloat+=float(opts.blocksize)/opts.pixelwidth
+    if outlenfloat >= outlen + 1:
+        out+=byte
+        outlen+=1
 
 if opts.width is None and opts.height is None and opts.ratio is None:
-    print "Trying to guess ration between 1:2 and 2:1 ..."
+    M=3
+    print "Trying to guess ratio between 1:%i and %i:1 ..." % (M, M)
 
     outmap=[]
     for i in range(len(ciphertext)/opts.blocksize):
         token=ciphertext[i*opts.blocksize:(i+1)*opts.blocksize].encode('hex')
         outmap+=['\xFF' if token == histo[0][0] else '\x00']
     # outmap is now a condensed view of the data where a 0xff byte represents most common block and 0x00 the other blocks
-    sq=int(math.sqrt(len(outmap)/pxblocksize))
+    sq=int(math.sqrt(len(outmap)/opts.blocksize*opts.pixelwidth))
     r={}
-    print "Width: from %i to %i\nStep: %i\nProgress:" % (sq/2*pxblocksize, sq*2*pxblocksize, opts.step),
-    for i in range(sq/2,sq*2):
+    print "Width: from %i to %i\nStep: %i\nProgress:" % (sq/M*opts.blocksize/opts.pixelwidth, sq*M*opts.blocksize/opts.pixelwidth, opts.step),
+    for i in range(sq/M,sq*M):
         if i % 100 == 0:
-           print i*pxblocksize,
+           print i*opts.blocksize/opts.pixelwidth,
            sys.stdout.flush()
         A=outmap[:-i:opts.step]
         B=outmap[i::opts.step]
@@ -116,26 +138,27 @@ if opts.width is None and opts.height is None and opts.ratio is None:
         r[i]=float(m)/(len(A))
     print ""
     r=sorted(r.iteritems(), key=operator.itemgetter(1), reverse=True)
-    opts.width = r[0][0]*pxblocksize
+    opts.width = int(r[0][0]*float(opts.blocksize)/opts.pixelwidth)
 
 if opts.ratio is not None:
     # Compute ratio
     ratio=tuple(map(int, opts.ratio.split(':')))
-    l=len(out)/4
+    l=len(out)
     x=math.sqrt(float(ratio[0])/ratio[1]*l)
     y=x/ratio[0]*ratio[1]
     xy=(int(x), int(y))
 
 if opts.width is not None:
-    xy=(opts.width, len(out)/4/opts.width)
+    xy=(opts.width, len(out)/opts.width)
 
 if opts.height is not None:
-    xy=(len(out)/4/opts.height, opts.height)
+    xy=(len(out)/opts.height, opts.height)
 
 print "Size: ", repr(xy)
 
 # Create image from output stream & ratio
-i=Image.fromstring(mode, xy,out)
+i=Image.fromstring('P', xy,out)
+i.putpalette(p)
 
 if opts.flip:
     i=i.transpose(Image.FLIP_TOP_BOTTOM)
